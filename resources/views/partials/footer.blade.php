e7ad7246f72e13c3918c6463d37dae338fb96968
Tony Cookey <tonyiroegbu59@gmail.com>
2019-03-20 15:29:54 +0100

    changed the footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer fixed-bottom">
+<footer class="footer sticky-bottom" style="margin-top:20px;">
         <div class="container">
           <p class="text-center text-muted" style="font-size:15px;" >Tone Designs &copy; 2019</p>
         </div>
