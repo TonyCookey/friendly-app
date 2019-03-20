d1501731e528b7af3728ee9c0d651beef7a9639f
Tony Cookey <tonyiroegbu59@gmail.com>
2019-03-20 15:18:39 +0100

    changed the footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,25 +1,5 @@
-<div class="footer-dark">
-  <footer>
-      <div class="container">
-          <div class="row">
-              <div class="col-sm-6 col-md-3 item">
-                  <h3>Services</h3>
-                  <ul>
-                      <li><a href="#">Web design</a></li>
-                      <li><a href="#">Development</a></li>
-                      <li><a href="#">Hosting</a></li>
-                  </ul>
-              </div>
-              <div class="col-sm-6 col-md-3 item">
-                  <h3>About</h3>
-                  <ul>
-                      <li><a href="about.html">Company</a></li>
-                      <li><a href="about.html">Team</a></li>
-                      <li><a href="#">Careers</a></li>
-                  </ul>
-              </div>
-              <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-linkedin-outline"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
-          </div>
-          <p class="copyright">Tone &nbsp;Designs © 2018</p>
-      </div>
-  </footer>
+<footer class="footer fixed-bottom">
+        <div class="container">
+          <p class="text-center text-muted" style="font-size:15px;" >Tone Designs &copy; 2019</p>
+        </div>
+</footer>
