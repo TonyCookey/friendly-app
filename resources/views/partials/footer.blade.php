3627ca99283ab26ce6a8c8bdf1b88ad046fcb294
Tony Cookey <tonyiroegbu59@gmail.com>
2019-03-20 15:08:01 +0100

    changed the footer error_2

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer static-bottom">
+<footer class="footer-copyright text-center py-3">
         <div class="container">
           <p class="text-center text-muted" style="font-size:15px;" >Tone Designs &copy; 2019</p>
         </div>
